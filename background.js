432ed4d3c2f27fac4f02d829da98cbf6d1c775a8
Nooraldin2001 <noorabdalkadir@std.mans.edu.eg>
2024-01-25 14:12:55 +0200

    adding intial project

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
     if (tab.url && tab.url.includes("youtube.com/watch")) {
       const queryParameters = tab.url.split("?")[1];
       const urlParameters = new URLSearchParams(queryParameters);
+      console.log(urlParameters);
   
       chrome.tabs.sendMessage(tabId, {
         type: "NEW",
@@ -9,4 +10,3 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
       });
     }
   });
-  
